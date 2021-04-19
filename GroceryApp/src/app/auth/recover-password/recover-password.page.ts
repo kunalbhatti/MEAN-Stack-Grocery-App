6cb6c7c83e5333f521bd5ba599000672126f01e3
Kunal Bhatti <kunal.bhatti15@gmail.com>
2021-04-19 15:24:10 +0530

    added email sent alert to password recover flow

--- GroceryApp/src/app/auth/recover-password/recover-password.page.ts
+++ GroceryApp/src/app/auth/recover-password/recover-password.page.ts
@@ -5,7 +5,15 @@ import {
 import {
   NgForm
 } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import {
+  Router
+} from '@angular/router';
+import {
+  AlertController
+} from '@ionic/angular';
+import {
+  AuthService
+} from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-recover-password',
@@ -14,14 +22,29 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RecoverPasswordPage implements OnInit {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+    private alertController: AlertController,
+    private router: Router) {}
 
   ngOnInit() {}
 
   recoverPassword(form: NgForm) {
     this.authService.recoverPassword(form.value.email).subscribe(
       (result) => {
-        console.log(result)
+        this.alertController.create({
+          header: 'Success',
+          message: 'Password reset link has been mailed to the email. Please follow the link to reset your password.',
+          buttons: [{
+            text: 'Ok',
+            handler: () => {
+              this.router.navigate(['/', 'auth', 'login']);
+            }
+          }]
+        }).then(
+          (alertEl: HTMLIonAlertElement) => {
+            alertEl.present();
+          }
+        );
       }
     );
   }
